38bd527f2f4971379d00c27df38c87242f19e169
EPECb <egor.onuchin@gmail.com>
2016-07-16 22:45:03 +0300

    Загушил код авторизации
    Для входа используется код "00000"

--- Code/PHP/authorization.php
+++ Code/PHP/authorization.php
@@ -1,11 +1,19 @@
 <?php
 session_start();
-$_POST['auth_code']=1;
-if(isset($_POST['auth_code'])){ //Авторизация клиента
-  echo json_encode($arr=array('session_id'=> session_id(), 'array'=>array('a'=>1,'b'=>2),'link'=>'PHP/menu.php'));
+$auth_code = $_POST['auth_code'];
+if(isset($auth_code){ //Авторизация клиента
+  if($auth_code=='00000'){
+    echo json_encode($arr=array('session_id'=> session_id(),'link'=>'PHP/menu.php'));
+  }else{
+    return_error();
+  }
 }elseif(isset($_POST['login']) AND isset($_POST['password'])){ //Авторизация оффика
   
 }else{
+  return_error();
+}
+
+function return_error(){
   echo json_encode($arr=array('session_id'=> session_id(), 'error_code'=>1, 'error_text'=>'Ошибка авторизации'));
 }
 ?>
