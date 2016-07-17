237e60f4974f3bd50ccf955d655644f2be0c5a27
EPECb <egor.onuchin@gmail.com>
2016-07-17 22:12:49 +0300

    Дописал авторизацию, добавил файл подключения к БД
    Теперь вместе с кодом авторизации нужно передавать код Торговой Точки. Параметр для передачи 'TT'. Опишу это в документах завтра. Заглушка на авторизацию клиента осталась - 00000

--- Code/PHP/authorization.php
+++ Code/PHP/authorization.php
@@ -1,19 +1,69 @@
 <?php
-session_start();
-$auth_code = $_POST['auth_code'];
-if(isset($auth_code)){ //Авторизация клиента
-  if($auth_code=='00000'){
-    echo json_encode($arr=array('session_id'=> session_id(),'link'=>'PHP/menu.php'));
-  }else{
-    return_error();
-  }
-}elseif(isset($_POST['login']) AND isset($_POST['password'])){ //Авторизация оффика
-  
-}else{
-  return_error();
+class Authorization{
+    public function __construct(){
+        $auth_code = filter_input(0, 'auth_code'); //Код авторизации клиента
+        $login = filter_input(0, 'login'); 
+        $password = md5('payforme'.filter_input(0,'password'));
+        $TT = filter_input(0,'TT');
+        include_once 'db_connect.php'; //Подключение к БД
+        
+        
+        if($login AND $password){ //Авторизация для персонала
+            $this.Employ_Authorization($login, $password);
+        }elseif ($auth_code AND $TT) { //Авторизация для клиента
+            if($auth_code=='00000'){
+                echo json_encode(array('link'=>'PHP/menu.php'));
+            }else{
+                $this.return_error();
+            }
+            //$this.Client_Autorization($auth_code,$TT);
+        }else{
+            $this->return_error();
+        }
+    }
+    
+    private function Client_Autorization($auth_code, $TT){//Авторизация для клиента
+        $res = $pdo->query("SELECT COUNT(*) FROM `AUTHORIZAION` "
+                . "WHERE "
+                . "`ENTER_CODE` = '$auth_code'"
+                . "AND `END` IS NULL"
+                . "AND `ID_TT` = '$TT'");
+        $res->setFetchMode(PDO::FETCH_ASSOC);
+        $row=$res->fetch();
+        $numrows=$row['count'];
+        if($numrows==1){
+            session_start();
+            echo json_encode(array('link'=>'PHP/menu.php'));
+        }else{
+            $this->return_error();
+        }
+    }
+    
+    private function Employ_Authorization($login, $password){//Авторизация для персонала
+        $res = $pdo->query("SELECT COUNT(ID_CONTACT),* FROM `CONTACT` "
+                . "WHERE "
+                . "`LOGIN` = '$login'"
+                . "AND `PASSWORD` = '$password'");
+        $res->setFetchMode(PDO::FETCH_ASSOC);
+        $row=$res->fetch();
+        $numrows=$row['count'];
+        if($numrows==1){
+            session_start();
+            if($row['role']=='WAITER'){
+                echo json_encode(array('link'=>'PHP/waiter.php'));
+            }else{
+                echo json_encode(array('link'=>'PHP/admin.php'));
+            }
+        }else{
+            $this->return_error();
+        }
+    }
+
+    private function return_error(){
+        echo json_encode(array('error_code'=>1, 'error_text'=>'Ошибка авторизации'));
+        exit();
+    }
 }
 
-function return_error(){
-  echo json_encode($arr=array('session_id'=> session_id(), 'error_code'=>1, 'error_text'=>'Ошибка авторизации'));
-}
+$obj = new Authorization;
 ?>
